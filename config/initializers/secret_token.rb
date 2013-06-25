6b0466120eb544682cf7499fbc8a998d893ee83a
claire allison <callison22@gmail.com>
2013-06-25 10:33:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kookopa::Application.config.secret_token = '090c39549aca3e113f3bd4ed7093cd9e11c6bb2e562ef7d0cba93cecfb5527d8839161d141d631c8cec25fdf4fe5dbe104966f343f996a8d7dd9c0a3d6db24da'
